4b025014cc5ed3d6110eb68d7f80fe0d854219a1
Kate <katy2jf@gmail.com>
2023-10-03 22:03:51 +0300

    add ssnf

--- pars.py
+++ pars.py
@@ -6,7 +6,13 @@ class Tree:
 
     def __str__(self):
         return str(self.cargo)
+    
 
+def check_char(token_list):
+    if token_list[0]!='|' and token_list[0]!='end' and token_list[0]!='*' and token_list[0]!=')':
+        return True
+    else:
+        return False
 
 def get_token(token_list, expected):
     if token_list[0] == expected:
@@ -32,22 +38,30 @@ def get_star(token_list):
     a = get_char(token_list)
     #print(token_list, "aa")
     if get_token(token_list, '*'):
+        subtree=a
         #print(token_list, "b")
-
+        while token_list[0]=='*':
+            helper=Tree('*', subtree, None)
+            subtree=helper
+            token_list[0:1] = []
         #b = get_product(token_list)
-        return Tree('*', a, None)
+        return Tree('*', subtree, None)
     else:
         return a
 
 def get_conc(token_list):
     a=get_star(token_list)
-    b=get_star(token_list)
-    return Tree('conc', a, b)
+    if check_char(token_list):
+        #print("hi", token_list)
+        b=get_alt(token_list)
+        return Tree('conc', a, b)
+    else:
+        return a
 
 def get_alt(token_list):
     a = get_conc(token_list)
     if get_token(token_list, '|'):
-        print("hhh")
+        #print("hhh")
         b = get_alt(token_list)
         return Tree('|', a, b)
     else:
@@ -58,14 +72,82 @@ def print_tree(tree):
     print (tree.cargo)
     print_tree(tree.left)
     print_tree(tree.right)
+
+def back(tree):
+    s=""
+    #print("fooooo", tree.cargo, type(tree.cargo))
+    if tree.left!=None:
+        left=back(tree.left)
+        #print('left', left, type(left))
+        right=None
+        if tree.right!=None: 
+            right=back(tree.right)
+        if tree.cargo=='conc':
+            s=left+right
+            return s
+        elif tree.cargo=='*':
+            if tree.left.cargo=='|' or tree.left.cargo == 'conc':
+                s="("+left+")"+"*"
+            else:
+                #print("gggggg", left)
+                s=left+"*"
+            return s
+        elif tree.cargo=='|':
+            s=left+'|'+right
+            return s
+        else:
+            #print("why")
+            return tree.cargo
+    else:
+        return str(tree.cargo)
+    
+def ssnf(tree):
+    if tree.cargo==None:
+        print("baaad")
+        return 
+    if tree.cargo!='conc' and tree.cargo!='|' and tree.cargo!='*':
+        return Tree(tree.cargo, None, None)
+    if tree.cargo=='|':
+        return Tree('|', ssnf(tree.left), ssnf(tree.right))
+    if tree.cargo=='conc':
+        return Tree('conc', ssnf(tree.left), ssnf(tree.right))
+    if tree.cargo == '*':
+        """print('llllllllllllllllllll0')
+        print_tree(ss(tree.left))
+        print('llllllllllllllllllll2')"""
+
+        return Tree('*', ss(tree.left), None)
+
+def ss(tree):
+    if tree.cargo==None:
+        return 
+    if tree.cargo!='conc' and tree.cargo!='|' and tree.cargo!='*':
+        #print("oo", tree.cargo)
+        return Tree(tree.cargo, None, None)
+    if tree.cargo=='|':
+        return Tree('|', ss(tree.left), ss(tree.right))
+    if tree.cargo=='conc':
+        if tree.left.cargo=='*' and tree.right.cargo=='*':
+            return Tree('|', ss(tree.left), ss(tree.right))
+        else:
+            #print("ppppp", ssnf(tree.left), ssnf(tree.right))
+            return Tree('conc', ssnf(tree.left), ssnf(tree.right))
+    if tree.cargo == '*':
+        """print("aaaaaaaaaaaaaaaaaaaaaaaa")
+        print_tree(ss(tree.left))
+        print(';;;;;;;;;;')"""
+        return ss(tree.left)
     
 regex=input()
 tok=list(regex)
-
 tok.append('end')
-print(tok)
-
-token_list = ['a', '|', 'b', '*', 'end']
+#print(tok)
+#token_list = ['a', '|', 'b', '*', 'end']
 #token_list = ['a, '|', b', 'end']
+
 tree = get_alt(tok)
-print_tree(tree)
+#print_tree(tree)
+#print('-----------------')
+tree2=ssnf(tree)
+#print_tree(tree2)
+print(back(tree2))
